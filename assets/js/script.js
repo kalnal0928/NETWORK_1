6412cb869de188476792c46606fac77f8af6a6f4
kalnal0928 <kalnal0928@gmail.com>
2025-08-05 14:45:16 +0900

    버그 픽스

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,9 +82,10 @@ function filterQuestions(selectedChapter, selectedType) {
     if (selectedType !== '선택하세요') {
                 filtered = filtered.filter(q => q.type === selectedType);
     }
+}
 
-    }
-
+// startQuiz 함수 수정
+function startQuiz(chapter, type) {
     // 필터링 실행
     if (!filterQuestions(chapter, type)) {
         return;
