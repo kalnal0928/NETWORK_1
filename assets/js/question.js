9fac45fe1df0b7f78760ee3cc1ef01198dec6d2c
kalnal0928 <kalnal0928@gmail.com>
2025-08-12 21:54:43 +0900

    변경

--- assets/js/question.js
+++ assets/js/question.js
@@ -179,7 +179,6 @@ const questions = [
         answer: "VHF",
         explanation: "초단파(very high frequency)는 VHF입니다.\n- VHF: Very High Frequency, 30 MHz ~ 300 MHz, 파장 약 1 m ~ 10 m\n- LF: Low Frequency, 30 kHz ~ 300 kHz, 파장 약 100 km ~ 1000 km\n- VLF: Very Low Frequency, 3 kHz ~ 30 kHz, 파장 약 10 km ~ 100 km\n- SHF: Super High Frequency, 3 GHz ~ 30 GHz, 파장 약 10 cm ~ 1 m",
 
-
         chapter: "네트워크",
         type: "multiple-choice"
     },
